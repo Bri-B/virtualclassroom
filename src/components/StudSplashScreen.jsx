5f95ce45d047f68cdce6f0ba4c758960a213437e
Bri-b <bri.skinner02@gmail.com>
2020-10-08 23:40:58 -0500

    (add) connection to assignment

--- src/components/StudSplashScreen.jsx
+++ src/components/StudSplashScreen.jsx
@@ -1,5 +1,6 @@
-import { Layout, Menu, Breadcrumb, Typography } from 'antd';
-import { UserOutlined, LaptopOutlined, NotificationOutlined } from '@ant-design/icons';
+import {
+  Layout, Typography,
+} from 'antd';
 
 import React from 'react';
 import PropTypes from 'prop-types';
@@ -7,22 +8,22 @@ import AnnouncementList from './allUsers/AnnouncementList';
 import AssignmentList from './allUsers/AssignmentList';
 import ClassList from './allUsers/ClassList';
 
-const { SubMenu } = Menu;
-const { Header, Content, Sider } = Layout;
-const {Title}= Typography;
+const { Content, Sider } = Layout;
+const { Title } = Typography;
 
 export default function StudSplashScreen({ name, user }) {
   return (
     <Layout>
-      <Sider width={200} className="site-layout-background">
-        <Title className="classSlide" style={{ color: 'white', backgroundColor: 'F0F2F5' }} level={3}>Class List</Title>
+      <Sider width={200} className="site-layout-background" style={{ backgroundColor: '#1890ff' }}>
+        <Title className="classSlide" style={{ color: 'white' }} level={3}>Class List</Title>
         <ClassList />
       </Sider>
       <Layout style={{ padding: '0 24px 24px' }}>
-        <Breadcrumb style={{ margin: '16px 0' }}>
-        </Breadcrumb>
-        <AnnouncementList />
-        <AssignmentList user={user} />
+        <Content>
+          <Title className="nameStudent" style={{ color: 'rgba(0, 0, 0, 0.85)' }} level={3}>{name}</Title>
+          <AnnouncementList />
+          <AssignmentList user={user} />
+        </Content>
       </Layout>
     </Layout>
   );
