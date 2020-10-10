df50b8b5c94a0571df9d2e29e7d9c665882f4cd5
Greg <gregthompson27@gmail.com>
2020-10-10 11:10:55 -0500

    (refactor) db initialization to include port from env

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -6,11 +6,12 @@ const user = process.env.DB_USERNAME;
 const host = process.env.DB_HOST;
 const database = process.env.DB_DBNAME;
 const password = process.env.DB_PASSWORD;
+const port = process.env.DB_PORT;
 
 const sequelize = new Sequelize(database, user, password, {
   host,
   dialect: 'postgres',
-  port: 5432,
+  port,
   logging: false,
 });
 
