0f8d13d70cf5e242b43c93b28a4349a0282d6b8e
Bri-b <bri.skinner02@gmail.com>
2020-10-09 22:27:46 -0500

    (refactor) add react routes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 /* eslint-disable react/jsx-filename-extension */
 // Initial location for react components to be added to index.html's "root" div
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+} from 'react-router-dom';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import 'antd/dist/antd.css';
+import TeachSplashScreen from './components/TeachSplashScreen';
+import StudSplashScreen from './components/StudSplashScreen';
 // import './styles.css';
 
 require('@babel/polyfill');
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <App />,
+  document.getElementById('root'),
+);
