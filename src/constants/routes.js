ca0324ae49756208f41d0325f2d0e0da1247982e
Bri-b <bri.skinner02@gmail.com>
2020-10-11 10:03:18 -0500

    (add) remaining teacher routes

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -5,16 +5,25 @@ const teacherRoot = `${baseURl}/teacher`;
 const studentRoot = `${baseURl}/student`;
 
 export const TEACHER_ROUTES = {
-  GET_TEACHER_BY_EMAIL: `${teacherRoot}/email/`, // :teacherEmail
-  GET_TEACHER_BY_ID: `${teacherRoot}/ID/`, // :teacherID
   GET_ALL_TEACHERS: `${teacherRoot}/`,
+  GET_TEACHER_BY_ID: `${teacherRoot}/ID/`, // :teacherID
+  GET_TEACHER_BY_EMAIL: `${teacherRoot}/email/`, // :teacherEmail
+  GET_TEACHER_BY_NAME: `${teacherRoot}/name/`, // :teacherName
   GET_ALL_CLASSES: `${teacherRoot}/classes/`, // :teacherID
   GET_SCHOOL_BY_TEACHID: `${teacherRoot}/school/`, // :teacherID
+  GET_ALL_ANNOUNCE_BY_CLASS: `${teacherRoot}/announcement/`, // :classID
+  GET_ALL_ASSIGN_BY_CLASS: `${teacherRoot}/assignment/`, // :classID
   PUT_UPDATE_CLASS: `${teacherRoot}/classes/`, // :classID
-  PUT_ADD_STUDENT: `${teacherRoot}/addStudentClass/`, // :studentID/:classID //?? should be post
+  PUT_EDIT_ASSIGN: `${teacherRoot}/edit/assignment/`, //
+  PUT_EDIT_ANNOUCE: `${teacherRoot}/edit/announcement/`, //
+  PUT_ADD_STUDENT_TO_CLASS: `${teacherRoot}/addStudentClass/`, // :studentID/:classID //
   DELETE_STUDENT_FROM_CLASS: `${teacherRoot}/deleteStudentClass/`, // :studentID/:classID
   DELETE_CLASS: `${teacherRoot}/classes/`, // :classID
+  DELETE_ANNOUNCE: `${teacherRoot}/delete/announcement/`, //
+  DELETE_ASSIGN: `${teacherRoot}/delete/assignment/`, //
+  // classes you want to associate with this announcement
   POST_ANNOUNCEMENT: `${teacherRoot}/create/announcement/`,
+  POST_ASSIGN: `${teacherRoot}/create/assignment/`,
   POST_ADD_CLASS: `${teacherRoot}/classes/`,
 };
 // need update student, delete assignment, delete announcement
